740bdc81e2b998bbaec7ee4bd76da8b125eb5345
RaibFlee <44354162+RaibFlee@users.noreply.github.com>
2019-08-22 10:36:52 +0800

    Create 3-2.c

---
+++ Chapter3/3-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*2.编写一个程序，要求提示输入一个ASCI码值(如，66)，然后打印
+输入的字符。*/
+
+int main() {
+	system("chcp 65001");
+	printf("请输入一个ACSII值:");
+	int i;
+	scanf("%d", &i);
+	printf("你输入的字符是:%c", i);
+	getchar();
+	return 0;
+}
