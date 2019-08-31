12919e386ebc9e1145c048b8fdd353ecc1420c3c
RaibFlee <44354162+RaibFlee@users.noreply.github.com>
2019-08-31 14:51:03 +0800

    Create 6-2.c

---
+++ Chapter6/6-2.c
@@ -0,0 +1,27 @@
+/*
+@filename:3-8.c
+@Author:RaibFlee
+@description:
+2.使用嵌套循环，按下面的格式打印字符:
+$
+$$
+$$$
+$$$$
+$$$$$
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	system("chcp 65001");
+	int i,j;
+	for(i=1;i<6;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("$");
+		}
+		printf("\n");
+	}
+	return 0;
+}
