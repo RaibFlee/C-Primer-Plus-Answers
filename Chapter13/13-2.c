d9bd2c7d37bc91ed7bc760e7a2c86a4e4f2332b5
RaibFlee <44354162+RaibFlee@users.noreply.github.com>
2019-10-20 21:49:14 +0800

    Create 13-2.c

---
+++ Chapter13/13-2.c
@@ -0,0 +1,39 @@
+/*
+@filename:13-2.c
+@Author:RaibFlee
+@description:
+2.编写一个文件拷贝程序，该程序通过命令行获取原始文件名和拷贝文
+件名。尽量使用标准I/O和二进制模式。
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *args[]) {
+	system("chcp 65001");
+	char x;
+	if (argc!=3){
+		printf("Usage:%s %s(original) %s(destination)\n",args[0],args[1],args[2]);
+		exit(EXIT_FAILURE);
+	}
+	FILE *f1,*f2;//f1:orginal,f2:destination
+	if ((f1=fopen(args[1],"rb"))==NULL){
+		printf("can't open %s\n",args[1]);
+		exit(EXIT_FAILURE);
+	}
+	if ((f2=fopen(args[2],"wb"))==NULL){
+		printf("can't create %s\n",args[2]);
+		exit(EXIT_FAILURE);
+	}
+	while (!feof(f1)){
+		fread(&x, sizeof(char),1,f1);
+		fwrite(&x, sizeof(char),1,f2);
+	}
+	fclose(f1);
+	fclose(f2);
+	printf("Done.\n");
+	return 0;
+}
+
